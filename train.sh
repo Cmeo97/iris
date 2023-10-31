436e0ff070bd4fd4a1c6ac0d7698935a236573a7
Cmeo97 <cristianmeo2@gmail.com>
2023-10-31 15:49:48 -0400

    sh files

--- train.sh
+++ train.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+#SBATCH --job-name=IRIS
+#SBATCH --partition=long                      
+#SBATCH --cpus-per-task=12
+#SBATCH --gres=gpu:rtx8000:1
+#SBATCH --mem=45G                                  
+#SBATCH --time=6-20:00:00
 
-conda activate iris
+conda activate irisXL
 
-task=BoxingNoFrameskip-v4 
+#task=BoxingNoFrameskip-v4 
 
-seed=0
+model=$1
+task=$2
+seed=$3
+exp_name=${model}'-'${task}'-'${seed}
 
 
-exp_name='irisXL-discrete-'${task}'-'${seed}
-
-
-nohup python src/main.py env.train.id=${task} wandb.name=${exp_name} common.seed=${seed} \
+nohup python src/main.py env.train.id=${task} world_model.model=${model} wandb.name=${exp_name} common.seed=${seed} \
 > 'logs/'${exp_name}'.out' 2> 'logs/'${exp_name}'.err'
